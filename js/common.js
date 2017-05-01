f53c626dd5fdbf61b0351226b5bb8f4f9e02ea27
Luckymalina <Happymalina@yandex.ru>
2017-05-01 22:08:18 +0300

    animate,scroll,menu

--- js/common.js
+++ js/common.js
@@ -28,5 +28,40 @@ $( document ).ready(function() {
         // ]
     });
 
+    $('input[type=tel]').mask("+7 (999) 999-99-99");
+
+    //nav to anchor
+    $('.header-menu__link').on('click', function() {
+
+        var scrollAnchor = $(this).attr('data-scroll'),
+            scrollPoint = $('section[data-anchor="' + scrollAnchor + '"]').offset().top - 10;
+
+        $('body,html').animate({
+            scrollTop: scrollPoint
+        }, 500);
+
+        return false;
+    });
+
+    $(window).scroll(function() {
+        var windscroll = $(window).scrollTop();
+        if (windscroll >= 100) {
+            $('.header-menu__list').addClass('fixed');
+            $('section').each(function(i) {
+                if ($(this).position().top <= windscroll - 10) {
+                    $('.header-menu__link.active').removeClass('active');
+                    $('.header-menu__link').eq(i).addClass('active');
+                }
+            });
+
+        } else {
+
+            $('.header-menu__list').removeClass('fixed');
+            $('.header-menu__link.active').removeClass('active');
+            $('.header-menu__link:first').addClass('active');
+        }
+
+    }).scroll();
+
 
 });
