4d6a66d13d23bca37f3e650db4b9158e152f1293
Luckymalina <Happymalina@yandex.ru>
2017-05-01 19:09:12 +0300

    done about and footer

--- js/common.js
+++ js/common.js
@@ -1,3 +1,32 @@
-/**
- * Created by Malina on 08.04.2017.
- */
+$( document ).ready(function() {
+
+
+    $('.portfolio-slider').slick({
+        // variableWidth: true,
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3
+        // responsive: [
+        //     {
+        //         breakpoint: 768,
+        //         settings: {
+        //             arrows: false,
+        //             centerMode: true,
+        //             centerPadding: '40px',
+        //             slidesToShow: 3
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 480,
+        //         settings: {
+        //             arrows: false,
+        //             centerMode: true,
+        //             centerPadding: '40px',
+        //             slidesToShow: 1
+        //         }
+        //     }
+        // ]
+    });
+
+
+});
